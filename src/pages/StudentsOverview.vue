27323a426e01017b22d2c1b64cc99f20d65f3f45
David Giardi <david.giardi.eng@gmail.com>
2020-05-26 00:43:24 +0200

    Add sample students table

--- src/pages/StudentsOverview.vue
+++ src/pages/StudentsOverview.vue
@@ -1,13 +1,71 @@
 <template>
   <q-page class="q-pa-lg">
-    <h4>Students</h4>
 
-    <p>Lorem ipsum</p>
+    <q-table
+      title="Students"
+      :data="data"
+      :columns="columns"
+      row-key="name"
+    />
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageStudentsOverview'
+  name: 'PageStudentsOverview',
+  data () {
+    return {
+      columns: [
+        {
+          name: 'firstname',
+          required: true,
+          label: 'First name',
+          align: 'left',
+          field: 'firstname',
+          sortable: true
+        },
+        { name: 'lastname', align: 'left', label: 'Last name', field: 'lastname', sortable: true },
+        { name: 'grade', label: 'Grade', field: 'grade', sortable: true },
+      ],
+      data: [
+        {
+          firstname: 'John',
+          lastname: 'Doey',
+          grade: 15,
+        },
+        {
+          firstname: 'Edward',
+          lastname: 'Skinsy',
+          grade: 20,
+        },
+        {
+          firstname: 'Julian',
+          lastname: 'Derek',
+          grade: 17,
+        },
+        {
+          firstname: 'Trevor',
+          lastname: 'Nik',
+          grade: 12,
+        },
+        {
+          firstname: 'Dave',
+          lastname: 'Koppf',
+          grade: 8,
+        },
+        {
+          firstname: 'Lorenz',
+          lastname: 'Ipsomus',
+          grade: 19,
+        },
+        {
+          firstname: 'Frank',
+          lastname: 'Herris',
+          grade: 5,
+        },
+      ]
+    }
+  }
 }
+
 </script>
