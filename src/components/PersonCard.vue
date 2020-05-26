0176569dd11ea75bcc9c3a3a026a943c014b3657
David Giardi <david.giardi.eng@gmail.com>
2020-05-26 19:24:11 +0200

    Add cards for persons on dashboard

---
+++ src/components/PersonCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-card bordered class="my-card" flat>
+    <q-item>
+      <q-item-section avatar>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+        </q-avatar>
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>{{fullname}}</q-item-label>
+        <q-item-label caption>
+          {{jobtitle}}
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <q-card-section class="q-pt-none">&nbsp;<br/>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'PersonCard',
+  props: {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    jobtitle: {
+      type: String,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style lang="sass">
+.my-card
+  width: 100%
+  max-width: 360px
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,13 +1,82 @@
 <template>
   <q-page class="q-pa-lg">
-    <h2>Dashboard home</h2>
+    <p class="text-h6">Instructors</p>
 
-    <p>Lorem ipsum</p>
+    <PersonCard v-for="(instructor, idx) in instructors" :key="idx" :fullname="instructor.name" :jobtitle="instructor.title"/>
+
+    <p class="text-h6 q-mt-md">Students</p>
+
+    <PersonCard v-for="(student, idx) in students" :key="idx" :fullname="student.name" :jobtitle="student.title"/>
+
+
   </q-page>
 </template>
 
 <script>
+import PersonCard from 'components/PersonCard'
+
 export default {
-  name: 'PageDashboard'
+  name: 'PageDashboard',
+
+  components: {
+    PersonCard
+  },
+
+  data() {
+    return {
+
+      instructors: [
+        {
+          name: 'John',
+          title: 'Maths instructor'
+        },
+        {
+          name: 'Jane',
+          title: 'Physics instructor'
+        },
+        {
+          name: 'Tony',
+          title: 'Coding instructor'
+        },
+      ],
+
+      students: [
+        {
+          name: 'Matt',
+          title: 'Student'
+        },
+        {
+          name: 'Josh',
+          title: 'Physics student'
+        },
+        {
+          name: 'Frank',
+          title: 'Software engineering student'
+        },
+        {
+          name: 'Heidi',
+          title: 'Software engineering student'
+        },
+        {
+          name: 'Stefan',
+          title: 'Business student'
+        },
+        {
+          name: 'Dave',
+          title: 'Mechanical engineering student'
+        },
+        {
+          name: 'Paul',
+          title: 'Software engineering student'
+        },
+      ],
+
+
+
+    }
+  }
 }
 </script>
+
+<style lang="sass">
+</style>
